9bfb34240e5f31bce53018394d45e3acd7ff0957
Anas <anasnew99@gmaiil.com>
2022-01-24 16:10:58 +0530

    committed from parent module

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const staticFolder = path.join(__dirname, "..", "react-frontend", "build");
 app.use("/", express.static(staticFolder));
 
 app.get("/", function (req, res) {
-  res.send("Fail to get front folder");
+  res.send("Fail to get build folder");
 });
 
 app.listen(3000, () => {
